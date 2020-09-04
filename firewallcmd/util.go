1f749a795aa573a9b7098b97579210e60fee47be
Prashant Gupta <prashantgupta24@gmail.com>
2020-09-03 17:24:05 -0700

    refactoring

--- firewallcmd/util.go
+++ firewallcmd/util.go
@@ -2,6 +2,7 @@ package firewallcmd
 
 import (
 	"fmt"
+	"net"
 	"os/exec"
 	"regexp"
 	"strings"
@@ -11,6 +12,11 @@ import (
 //example:
 //firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" source address="10.10.99.10/32" port protocol="tcp" port="22" accept'
 func EnableRichRuleForIP(ipAddr string) (string, error) {
+
+	//check if valid ipv4 address
+	if !isValidIpv4(ipAddr) {
+		return "", fmt.Errorf("not a valid IPv4 address : %v", ipAddr)
+	}
 	cmd1 := exec.Command(`firewall-cmd`, `--permanent`, "--zone=public", `--add-rich-rule=rule family="ipv4" source address="`+ipAddr+`/32" port protocol="tcp" port="22" accept`)
 	//uncomment for debugging
 	// for _, v := range cmd1.Args {
@@ -63,23 +69,21 @@ func GetIPSInFirewall() ([]string, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error while fetching IPs from firewall-cmd, err: %v", err)
 	}
-	richRules := string(output)
-	//rule family="ipv4" source address="73.223.28.39/32" port port="22" protocol="tcp" accept
-	//rule family="ipv4" source address="73.223.28.40/32" port port="22" protocol="tcp" accept
-
-	ruleLines := strings.Split(richRules, "\n")
 
 	stringToSearch := "address=\""
-	for _, rule := range ruleLines {
-		//fmt.Println(line)
-		//r, _ := regexp.Compile("source address=\"[0-9]*(.)")
+	richRuleLines := strings.Split(string(output), "\n")
+
+	for _, rule := range richRuleLines {
 		r, _ := regexp.Compile(stringToSearch + "[0-9.]*")
 		ipAddr := strings.TrimPrefix(r.FindString(rule), stringToSearch)
-		if ipAddr != "" {
+		if isValidIpv4(ipAddr) {
 			ipsInFirewall = append(ipsInFirewall, ipAddr)
-			fmt.Println(ipAddr)
+			//fmt.Println(ipAddr)
 		}
 	}
-
 	return ipsInFirewall, err
+}
+
+func isValidIpv4(host string) bool {
+	return net.ParseIP(host) != nil
 }
